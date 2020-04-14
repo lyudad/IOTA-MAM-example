88cf2d0299f6899479a80ab3e84aec5dd49ea09e
mac <mac@email.com>
2020-04-14 16:21:19 +0300

    fix dependencies

--- sendID.js
+++ sendID.js
@@ -5,6 +5,13 @@ const PublishAll = require('./PublishAll.js')
 const seed = 'IOWPPONJTYHHVRFRGFRWRVQU9VDMQYLEVVEABJPW9PT9F9SYMGIOFDXPYSLGESFTMBSJECQEPLHGWWYRZ'
 const address = 'YEHIA9TA9COPCTMOIFUEJVGUNDLSSJQCORYMEAZHRAUJXH9HGNI99BMLQDQOZNCLNVOLFMSFUIVPMYXDWEZFSZQROC'
 
+// iota.getNewAddress(seed, { index: 0, securityLevel: 2, total: 1 })
+//   .then(address => {
+//     console.log('Your address is: ' + address);
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   });
 
 SendMamRestricted.execute("initialize root").then(function(result) {
     SendPublicTransaction.execute(seed,address,result)
